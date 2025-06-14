863d0c6dbe0c466ee3094749d642bc78d066bc09
Rifqi Irfansyah <rifqiirfansyah518@gmail.com>
2025-06-15 01:02:02 +0700

    feat: add landing page

---
+++ resources/views/landing_page/landingPage.blade.php
@@ -0,0 +1,73 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <title>PeakForm</title>
+  <link rel="stylesheet" href="{{ asset('styles.css') }}">
+</head>
+<body>
+  <header>
+    <nav class="navbar">
+      <div class="logo">PeakForm</div>
+      <ul class="nav-links">
+        <li><a href="#">Home</a></li>
+        <li><a href="#">Features</a></li>
+        <li><a href="#">Pricing</a></li>
+      </ul>
+    </nav>
+  </header>
+
+  <section class="hero">
+    <div class="hero-text">
+      <h1>PeakForm</h1>
+      <p>Let's make your workkout more organize and easily using the PeakForm Apps with featuring guided exercises, goal tracking, and smart notifications to keep you on track every day</p>
+      <a href="">
+        <img class="download-icon" src="asset/download.png" alt="Goal" class="feature-icon"/>
+      </a>
+    </div>
+    <div class="hero-image">
+      <img src="asset/apps.png" alt="Phone Illustration" />
+    </div>
+  </section>
+
+  <section class="features">
+  <h2 class="features-title">FEATURES</h2>
+    <div class="feature-item">
+      <img src="asset/goal.png" alt="Goal" class="feature-icon" />
+      <div class="feature-text">
+        <h2 class="title-feature">Achieve Your<br>Goal</h3>
+        <p>Our exercise depends on your achieve goal, we provide goal in build muscle and endurance.</p>
+      </div>
+    </div>
+
+    <div class="feature-item">
+      <img src="asset/rank.png" alt="Rank" class="feature-icon" />
+      <div class="feature-text">
+        <h2 class="title-feature">Rank</h3>
+        <p>Our exercise depends on your achieve goal, we provide goal in build muscle and endurance.</p>
+      </div>
+    </div>
+
+    <div class="feature-item">
+      <img src="asset/notif.png" alt="Notification" class="feature-icon" />
+      <div class="feature-text">
+        <h2 class="title-feature">Notification</h3>
+        <p>Our exercise depends on your achieve goal, we provide goal in build muscle and endurance.</p>
+      </div>
+    </div>
+
+    <div class="feature-item">
+      <img src="asset/custom.png" alt="Customization" class="feature-icon" />
+      <div class="feature-text">
+        <h2 class="title-feature">Customization</h3>
+        <p>Our exercise depends on your achieve goal, we provide goal in build muscle and endurance.</p>
+      </div>
+    </div>
+</section>
+
+  <footer>
+    <p>Terms and Privacy • Privacy Policy</p>
+  </footer>
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -3,11 +3,16 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ExerciseController;
 
+Route::get('/', function() {return view('landing_page.landingPage');})->name('landingpage');
 Route::get('/admin', function () {
     return view('welcome');
 });
 
-Route::get('/exercises', [ExerciseController::class, 'index'])->name('exercises.index');
+Route::get('/login', [AuthController::class, 'showLoginForm'])->name('login');
+Route::post('/login', [AuthController::class, 'login']);
+Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
+
+Route::get('/exercises', [ExerciseController::class, 'index'])->middleware('auth')->name('exercises.index');
 Route::get('/exercises/create', [ExerciseController::class, 'create'])->name('exercises.create');
 Route::post('/exercises', [ExerciseController::class, 'store'])->name('exercises.store');
 Route::get('/exercises/{id}', [ExerciseController::class, 'show'])->name('exercises.show');
